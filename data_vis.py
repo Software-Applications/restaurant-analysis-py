af02bd39db74af5145ff9641ad5162888c598463
DheerajRekula <47189595+DheerajRekula@users.noreply.github.com>
2019-06-29 16:36:44 -0400

    distribution

--- data_vis.py
+++ data_vis.py
@@ -8,7 +8,9 @@ from pprint import pprint
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import io
+#import altair as alt
 
 #import plotly
 #import plotly.plotly as py
@@ -65,10 +67,10 @@ plt.savefig(bytes_image, format='png')
 plt.show()
 '''
 #################################################################################3
-
-user_input = input('Select a cuisine type')
-
-if user_input == 'all':
+#user_input = input('Select a cuisine type - ')
+########################working code. Better than above. You can delete above#######################
+'''
+if user_input.lower() == 'all':
     category_count = bs_data.groupby(['rating']).size().reset_index(name='counts')
 else:
     bs_data_f = bs_data[bs_data['category'] == user_input]
@@ -86,6 +88,45 @@ plt.xlabel('Quality Rating')
 plt.ylabel('# of Restaurants')
 plt.title(f"How did {user_input} restaurants perform??")
 plt.show()
+'''
+#########################################################################################33
+bs_review = bs_data[['category', 'review_count']]
+bs_rating = bs_data[['category', 'rating']]
+
+bs_review_agg = bs_review.groupby(['category']).sum().reset_index()
+bs_rating_agg = bs_rating.groupby(['category']).mean().reset_index()
+
+####################Working. Fix color coding################################3
+'''
+
+
+
+categories = list(bs_data['category'].unique())
+#new_cmap = plt.rand_cmap(100, type='bright', first_color_black=True, last_color_black=False, verbose=True)
+colors = ['blue', 'crimson', 'deeppink', 'violet', 'c', 'olive', 'grey', 'plum', 'palegreen', 'sienna', 'navy', 'darkcyan', 'hotpink', 'pink', 'indianred', 'magenta', 'purple', 'brown', 'dimgrey', 'g']
+
+for cat, col in zip(categories, colors):
+    bs_review_s = bs_review[bs_review['category'] == cat]
+    bs_rating_s = bs_rating[bs_rating['category'] == cat]
+    c_bs_rr = pd.concat([bs_review, bs_rating], axis = 1)
+    y_pos = c_bs_rr['rating']
+    x_pos = c_bs_rr['review_count']
+    plt.scatter(x_pos, y_pos, c = col, marker = 'o', s=1, label = cat)
+    plt.legend()
+plt.show()
+'''
+###############################################################################
+
+
+pop_category = bs_review_agg.sort_values(by = 'review_count', ascending = False)
+y_pos = pop_category['category']
+x_pos = pop_category['review_count']
+plt.barh(y_pos, x_pos, align='center')
+plt.xlabel('Quality Rating')
+plt.ylabel('# of Restaurants')
+#plt.title(f"How did {user_input} restaurants perform??")
+plt.show()
+
 
 
 
