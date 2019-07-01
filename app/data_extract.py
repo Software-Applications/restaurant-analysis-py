9c5b64243195850f0b2804af3818f7187cf7b105
DheerajRekula <47189595+DheerajRekula@users.noreply.github.com>
2019-07-01 12:49:18 -0400

    updated data extract and data vis code

--- app/data_extract.py
+++ app/data_extract.py
@@ -19,14 +19,13 @@ import time
 import sys
 import sendgrid
 from sendgrid.helpers.mail import *
-#from send_email_notification import send_email
 
 ###############################
 # loading environment variables
 ###############################
 
 #> loads contents of the .env file into the script's environment
-load_dotenv() 
+load_dotenv()
 
 #> assign the API_Key to a variable and create a headers variable that can be used for authentication.
 #  headers variable will be used to pass requests to YELP Fusion APIs
@@ -90,160 +89,162 @@ def send_email(text):
     response = sg.client.mail.send.post(request_body=mail.get())
     return response
 
-#####################################################################################
-#> STEP 1 : PULL REQUIRED DATA USING BUSINESS SEARCH, REVIEW APIS
-#####################################################################################
+if __name__ == "__main__":
+    
+    #####################################################################################
+    #> STEP 1 : PULL REQUIRED DATA USING BUSINESS SEARCH, REVIEW APIS
+    #####################################################################################
 
-#> define your cuisines
-#cuisines = ['indian']
-cuisines = ['indian', 'chinese', 'italian', 'japanese', 'mediterranean', 'thai', 'american', 'greek', 'fusion', 'korean', 'french', 'spanish', 'middle_eastern', 'moroccan', 'turkish', 'lebanese', 'peruvian', 'german', 'african', 'caribbean']
+    #> define your cuisines
+    #cuisines = ['indian']
+    cuisines = ['indian', 'chinese', 'italian', 'japanese', 'mediterranean', 'thai', 'american', 'greek', 'fusion', 'korean', 'french', 'spanish', 'middle_eastern', 'moroccan', 'turkish', 'lebanese', 'peruvian', 'german', 'african', 'caribbean']
 
 
-#######################
-#> BUSINESS SEARCH API
-#######################
-# deletes the old dataset of business_search, and creates a new one with same name
+    #######################
+    #> BUSINESS SEARCH API
+    #######################
+    # deletes the old dataset of business_search, and creates a new one with same name
 
-try:
-    google_sheets_cleanup('business_search', 10, 7, header = ['alias', 'category', 'restaurant_id', 'id_closed', 'name','rating', 'review_count', 'update_date'])
-except:
-    err_text = " Business Reviews dataset has been sucessfully truncated"
-    send_email(err_text)
-    err_cd = 15
-    sys.exit(err_cd)
-
-time.sleep(105)
-
-# need to accumulate all businesses ids for reviews api
-ids = []
-#> BUSINESS SEARCH API. USED TO EXTRACT BUSINESS SEARCH DATA
-url_search='https://api.yelp.com/v3/businesses/search'
-google_search_index = 2
-for cuisine in cuisines:
-    ###  searches based on cuisine type in New York City
-    params_search = {'term':cuisine,'location':'New York City'}
-
-    ###  Making a get request to the API
-    req_search=requests.get(url_search, params=params_search, headers=headers)
-
-    ### checks whether the get request is successful or not
     try:
-        req_search.status_code == 200
+        google_sheets_cleanup('business_search', 10, 7, header = ['alias', 'category', 'restaurant_id', 'id_closed', 'name','rating', 'review_count', 'update_date'])
     except:
-        err_text = "Connection to yelp api failed. the is no input available for data processing. contact dheeraj rekula"
+        err_text = " Business Search dataset could not be truncated"
         send_email(err_text)
-        err_cd = 5
-        sys.exit(5)
+        err_cd = 15
+        sys.exit(err_cd)
 
-    ### converting into json format 
-    business_search = json.loads(req_search.text)
+    time.sleep(105)
 
-    ### reading only relevant data
-    restaurants = business_search["businesses"]
-    rows_business = []
-    for restaurant in restaurants:
-        row = {
-            'alias': restaurant['alias'],
-            'categories': cuisine.lower(),
-            'id': restaurant['id'],
-            'id_closed': restaurant['is_closed'],
-            'name': restaurant['name'],
-            'rating': restaurant['rating'],
-            'review_count': restaurant['review_count'],
-            'update_date' : cur_date()
-        }
-        rows_business.append(row)
-        # useful for reviews api below
-        ids.append(row['id'])
+    # need to accumulate all businesses ids for reviews api
+    ids = []
+    #> BUSINESS SEARCH API. USED TO EXTRACT BUSINESS SEARCH DATA
+    url_search='https://api.yelp.com/v3/businesses/search'
+    google_search_index = 2
+    for cuisine in cuisines:
+        ###  searches based on cuisine type in New York City
+        params_search = {'term':cuisine,'location':'New York City'}
+
+        ###  Making a get request to the API
+        req_search=requests.get(url_search, params=params_search, headers=headers)
+
+        ### checks whether the get request is successful or not
+        try:
+            req_search.status_code == 200
+        except:
+            err_text = "Connection to yelp api failed. the is no input available for data processing. contact dheeraj rekula"
+            send_email(err_text)
+            err_cd = 5
+            sys.exit(5)
+
+        ### converting into json format 
+        business_search = json.loads(req_search.text)
+
+        ### reading only relevant data
+        restaurants = business_search["businesses"]
+        rows_business = []
+        for restaurant in restaurants:
+            row = {
+                'alias': restaurant['alias'],
+                'categories': cuisine.lower(),
+                'id': restaurant['id'],
+                'id_closed': restaurant['is_closed'],
+                'name': restaurant['name'],
+                'rating': restaurant['rating'],
+                'review_count': restaurant['review_count'],
+                'update_date' : cur_date()
+            }
+            rows_business.append(row)
+            # useful for reviews api below
+            ids.append(row['id'])
+        try:
+            for row in rows_business:                
+                row_value = [v for v in row.values()]
+                google_sheets_data('business_search', row_value, google_search_index)
+                google_search_index = google_search_index + 1
+                time.sleep(1)
+                if google_search_index % 49 == 0:
+                    time.sleep(105)
+                else:
+                    pass
+            pass_text = f"Data upload of business search for {cuisine} cuisine succeeded. If you find any issues contact dheeraj rekula"
+            send_email(pass_text)
+        except:
+            fail_text = f"Data upload of business search for {cuisine} cuisine failed. contact the dheeraj rekula"
+            send_email(fail_text)
+            err_cd = 101
+            sys.exit(err_cd)
+
+    #r = google_sheets_data_del('business_search')
+    #breakpoint()
+
+    #######################
+    #> BUSINESS REVIEWS API
+    #######################
+    ### reviews api is https://api.yelp.com/v3/businesses/{id}/reviews. 
+    ### it requires a business id to work. business ids are available in 'ids' list
+
+    time.sleep(105)
+
+    # removes old dataset of business_reviews
     try:
-        for row in rows_business:                
+        google_sheets_cleanup('business_reviews', 10, 4, header = ['restaurant_id', 'review_id', 'user_rating', 'text', 'update_date'])
+    except:
+        err_text = " Business Reviews dataset could not be truncated"
+        send_email(err_text)
+        err_cd = 15
+        sys.exit(err_cd)
+
+    time.sleep(105)
+    
+    rows_reviews=[]
+    for id in ids:
+        url_reviews = f"https://api.yelp.com/v3/businesses/{id}/reviews"
+        req_reviews=requests.get(url_reviews, headers=headers)
+        # email api error
+        try:
+            req_reviews.status_code == 200
+        except:
+            err_text = "Connection to yelp reviews api failed. the is no input available for data processing. contact dheeraj rekula"
+            send_email(err_text)
+            err_cd = 6
+            sys.exit(5)
+        business_review = json.loads(req_reviews.text)
+        review_details = business_review['reviews']
+
+        # reading only relevant data
+        for detail in review_details:
+            row = {
+                'restaurant_id' : id,
+                'review_id' : detail['id'],
+                'user_rating' : detail['rating'],
+                'text' : detail['text'],
+                'update_date' : cur_date()
+            }
+            rows_reviews.append(row)
+
+    # writes review data to gsheets
+    google_review_index = 2
+
+    try:
+        for row in rows_reviews:
+            # doing this because i created a list of dictionaries above, as i am comfortable working with dictionaries
             row_value = [v for v in row.values()]
-            google_sheets_data('business_search', row_value, google_search_index)
-            google_search_index = google_search_index + 1
+            google_sheets_data('business_reviews', row_value, google_review_index)
+            google_review_index = google_review_index + 1
+            # the following lines are important to ensure i maintain time restrictions on google drive api
             time.sleep(1)
-            if google_search_index % 49 == 0:
+            if google_review_index % 49 == 0:
                 time.sleep(105)
             else:
                 pass
-        pass_text = f"Data upload of business search for {cuisine} cuisine succeeded. If you find any issues contact dheeraj rekula"
+
+        pass_text = "Data upload of business reviews succeeded. if you find any issues contact dheeraj rekula"
         send_email(pass_text)
     except:
-        fail_text = f"Data upload of business search for {cuisine} cuisine failed. contact the dheeraj rekula"
+        fail_text = "Data upload of business search failed. contact the dheeraj rekula"
         send_email(fail_text)
-        err_cd = 101
-        sys.exit(err_cd)
-
-#r = google_sheets_data_del('business_search')
-#breakpoint()
-
-#######################
-#> BUSINESS REVIEWS API
-#######################
-### reviews api is https://api.yelp.com/v3/businesses/{id}/reviews. 
-### it requires a business id to work. business ids are available in 'ids' list
-
-time.sleep(105)
-
-# removes old dataset of business_reviews
-try:
-    google_sheets_cleanup('business_reviews', 10, 4, header = ['restaurant_id', 'review_id', 'user_rating', 'text', 'update_date'])
-except:
-    err_text = " Business Reviews dataset has been sucessfully truncated"
-    send_email(err_text)
-    err_cd = 15
-    sys.exit(err_cd)
-
-time.sleep(105)
-  
-rows_reviews=[]
-for id in ids:
-    url_reviews = f"https://api.yelp.com/v3/businesses/{id}/reviews"
-    req_reviews=requests.get(url_reviews, headers=headers)
-    # email api error
-    try:
-        req_reviews.status_code == 200
-    except:
-        err_text = "Connection to yelp reviews api failed. the is no input available for data processing. contact dheeraj rekula"
-        send_email(err_text)
-        err_cd = 6
-        sys.exit(5)
-    business_review = json.loads(req_reviews.text)
-    review_details = business_review['reviews']
-    
-    # reading only relevant data
-    for detail in review_details:
-        row = {
-            'restaurant_id' : id,
-            'review_id' : detail['id'],
-            'user_rating' : detail['rating'],
-            'text' : detail['text'],
-            'update_date' : cur_date()
-        }
-        rows_reviews.append(row)
-
-# writes review data to gsheets
-google_review_index = 2
-
-try:
-    for row in rows_reviews:
-        # doing this because i created a list of dictionaries above, as i am comfortable working with dictionaries
-        row_value = [v for v in row.values()]
-        google_sheets_data('business_reviews', row_value, google_review_index)
-        google_review_index = google_review_index + 1
-        # the following lines are important to ensure i maintain time restrictions on google drive api
-        time.sleep(1)
-        if google_review_index % 49 == 0:
-            time.sleep(105)
-        else:
-            pass
-
-    pass_text = "Data upload of business reviews succeeded. if you find any issues contact dheeraj rekula"
-    send_email(pass_text)
-except:
-    fail_text = "Data upload of business search failed. contact the dheeraj rekula"
-    send_email(fail_text)
-    err_cd = 102
-    sys.exit(102)
+        err_cd = 102
+        sys.exit(102)
 
 
 
--- app/data_vis.py
+++ app/data_vis.py
@@ -10,8 +10,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import io
+import sys
+from itertools import chain
 from wordcloud import WordCloud, STOPWORDS
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 #import altair as alt
 
 #import plotly
@@ -48,147 +51,212 @@ def savefile(fileandext):
         os.remove(file_name)
     return file_name
 
-### Defining the initial datasets
+def float_format(data):
+    x = "{0:.2f}".format(data)
+    return x
 
-business_search = google_sheets_data('business_search')
-business_reviews = google_sheets_data('business_reviews')
-bs_data = pd.DataFrame(business_search)
-br_data = pd.DataFrame(business_reviews)
+if __name__ == "__main__":
+    
+    ### Defining the initial datasets
 
-bs_review = bs_data[['category','restaurant_id', 'review_count']]
-bs_rating = bs_data[['category', 'restaurant_id', 'rating']]
-bs_review_agg = bs_review.groupby(['category']).sum().reset_index()
-bs_rating_agg = bs_rating.groupby(['category']).mean().reset_index()
+    business_search = google_sheets_data('business_search_bk')
+    business_reviews = google_sheets_data('business_reviews_bk')
+    bs_data = pd.DataFrame(business_search)
+    br_data = pd.DataFrame(business_reviews)
+    cumm_bs_br = bs_data.merge(br_data, on = 'restaurant_id', how = 'inner')
 
-###################################################################################
-### Bar Chart - How good are restaurants in general?
-# creates a bar chart based on user input
+    bs_review = bs_data[['category','restaurant_id', 'review_count']]
+    bs_rating = bs_data[['category', 'restaurant_id', 'rating']]
+    bs_review_agg = bs_review.groupby(['category']).sum().reset_index()
+    bs_rating_agg = bs_rating.groupby(['category']).mean().reset_index()
 
+    colors = ['blue', 'crimson', 'deeppink', 'violet', 'c', 'olive', 'grey', 'plum', 'palegreen', 'sienna', 'navy', 'darkcyan', 'hotpink', 'pink', 'indianred', 'magenta', 'purple', 'brown', 'dimgrey', 'g']
+    cuisine_types = bs_data.category.unique().tolist()
+    ###################################################################################
+    ### Bar Chart - How good are restaurants in general?
+    # creates a bar chart based on user input
 
-category_count = bs_data.groupby(['rating']).size().reset_index(name='counts')
-category_count['quality'] = category_count['rating'].apply(quality)
-del(category_count['rating'])
-category_agg = category_count.groupby(['quality']).sum().reset_index()
-category_agg['quality'] = pd.Categorical(category_agg['quality'], ['Exceptional', 'Very Good', 'Average', 'Poor'])
-category_agg.sort_values(by = 'quality', inplace = True)
-x_pos = category_agg['quality']
-y_pos = category_agg['counts']
-bars = plt.bar(x_pos, y_pos, align='center')
-plt.xlabel('Quality Rating')
-plt.ylabel('# of Restaurants')
-plt.title(f"How did different cuisines perform??")
-for bar in bars:
-    yval = bar.get_height()
-    plt.text(bar.get_x() + 0.3, yval + 5, yval, horizontalalignment='left')
-plt.grid(axis = 'y', which='minor')
-plt.savefig(savefile('restaurantcount.png'), bbox_inches='tight')
-plt.clf()
+    category_count = bs_data.groupby(['rating']).size().reset_index(name='counts')
+    category_count['quality'] = category_count['rating'].apply(quality)
+    del(category_count['rating'])
+    category_agg = category_count.groupby(['quality']).sum().reset_index()
+    category_agg['quality'] = pd.Categorical(category_agg['quality'], ['Exceptional', 'Very Good', 'Average', 'Poor'])
+    category_agg.sort_values(by = 'quality', inplace = True)
+    x_pos = category_agg['quality']
+    y_pos = category_agg['counts']
+    bars = plt.bar(x_pos, y_pos, align='center')
+    plt.xlabel('Quality Rating')
+    plt.ylabel('# of Restaurants')
+    plt.title(f"How did different cuisines perform??")
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + 0.3, yval + 5, yval, horizontalalignment='left')
+    plt.grid(axis = 'y', which='minor')
+    plt.savefig(savefile('1 restaurantcount.png'), bbox_inches='tight')
+    plt.clf()
 
-#TODO: Background color, format axis labels, and text above each bar
+    #TODO: Background color, format axis labels, and text above each bar
 
-#########################################################################################
-### HBar Chart - Popular Cusines
-# Assuming the one with highest reviews is more popular
-pop_category = bs_review_agg.sort_values(by = 'review_count', ascending = False)
-y_pos = pop_category['category']
-x_pos = pop_category['review_count']
-bars = plt.barh(y_pos, x_pos, align='center')
-plt.xlabel('# of Reviews')
-plt.ylabel('Cusine Type')
-plt.title("What are the most popular cuisines?")
-for bar in bars:
-    xval = bar.get_width()
-    plt.text(xval,bar.get_y(),xval)
-plt.grid(axis = 'x', which='minor')
-plt.savefig(savefile('mostreviews.png'), bbox_inches='tight')
-plt.clf()
-#TODO: Background color, format axis labels, and text next to each bar
-#############################################################################################
-colors = ['blue', 'crimson', 'deeppink', 'violet', 'c', 'olive', 'grey', 'plum', 'palegreen', 'sienna', 'navy', 'darkcyan', 'hotpink', 'pink', 'indianred', 'magenta', 'purple', 'brown', 'dimgrey', 'g']
-cumm_bs_rr = bs_review_agg.merge(bs_rating_agg, on = 'category', how = 'inner')
-categories = list(cumm_bs_rr['category'])
-#breakpoint()
-metrics = []
-for i in range(0,20,1):
-    rating = cumm_bs_rr.iloc[i]['rating']
-    review = cumm_bs_rr.iloc[i]['review_count']
-    lst = [rating, review]
-    metrics.append(lst)
-#breakpoint()
+    #########################################################################################
+    ### HBar Chart - Popular Cusines
+    # Assuming the one with highest reviews is more popular
 
-for metric, color, category in zip(metrics, colors, categories):
-    y, x = metric
-    plt.scatter(x, y, alpha = 0.8, marker = '*', c = color, s = 30, label = category)
-    plt.text(x-500, y-0.01, category, fontsize = 6)
+    pop_category = bs_review_agg.sort_values(by = 'review_count', ascending = False)
+    y_pos = pop_category['category']
+    x_pos = pop_category['review_count']
+    bars = plt.barh(y_pos, x_pos, align='center')
+    plt.xlabel('# of Reviews')
+    plt.ylabel('Cusine Type')
+    plt.title("What are the most popular cuisines?")
+    for bar in bars:
+        xval = bar.get_width()
+        plt.text(xval,bar.get_y(),xval)
+    plt.grid(axis = 'x', which='minor')
+    plt.savefig(savefile('2 mostreviews.png'), bbox_inches='tight')
+    plt.clf()
+    #TODO: Background color, format axis labels, and text next to each bar
+    #############################################################################################
+    ### Scatter Plot to show reviews and ratings in a single view to get deeper insight into what are the best performing cusines
+    # Assuming higher number of reviews indicates more popularity
+    cumm_bs_rr = bs_review_agg.merge(bs_rating_agg, on = 'category', how = 'inner')
+    categories = list(cumm_bs_rr['category'])
     #breakpoint()
-plt.xlabel('# of Reviews')
-plt.ylabel('Avg. Rating')
-plt.title("The best restaurants")
+    metrics = []
+    for i in range(0,20,1):
+        rating = cumm_bs_rr.iloc[i]['rating']
+        review = cumm_bs_rr.iloc[i]['review_count']
+        lst = [rating, review]
+        metrics.append(lst)
 
-min_rev = min(cumm_bs_rr['review_count'])
-max_rev = max(cumm_bs_rr['review_count'])
-mid_rev = (min_rev + max_rev)/2
-min_rat = min(cumm_bs_rr['rating'])
-max_rat = max(cumm_bs_rr['rating'])
-mid_rat = (min_rat + max_rat)/2
+    for metric, color, category in zip(metrics, colors, categories):
+        y, x = metric
+        plt.scatter(x, y, alpha = 0.8, marker = '*', c = color, s = 30, label = category)
+        plt.text(x-500, y-0.01, category, fontsize = 6)
 
-plt.xlim((min_rev, max_rev))
-plt.ylim((min_rat, max_rat))
-plt.plot([mid_rev,mid_rev],[min_rat-1000,max_rat+1000], linewidth=1, color='gray' )
-plt.plot([min_rev-1000,max_rev+1000],[mid_rat,mid_rat], linewidth=1, color='gray' )
+    plt.xlabel('# of Reviews')
+    plt.ylabel('Avg. Rating')
+    plt.title("The best restaurants")
 
-plt.annotate('Leaders', xy=(mid_rev, mid_rat+0.02), fontsize = 6, c = 'red')
-plt.annotate('Popular', xy=(mid_rev, min_rat+0.02), fontsize = 6, c = 'red')
-plt.annotate('Highly Rated', xy=(min_rev, mid_rat+0.02), fontsize = 6, c = 'red')
-plt.annotate('Promising', xy=(min_rev, min_rat+0.02), fontsize = 6, c = 'red')
-plt.savefig(savefile('scatter.png'), bbox_inches='tight')
-plt.clf()
-#TODO: Clean up labels
+    min_rev = min(cumm_bs_rr['review_count'])
+    max_rev = max(cumm_bs_rr['review_count'])
+    mid_rev = (min_rev + max_rev)/2
+    min_rat = min(cumm_bs_rr['rating'])
+    max_rat = max(cumm_bs_rr['rating'])
+    mid_rat = (min_rat + max_rat)/2
 
-#################Working. Just use adjective
+    plt.xlim((min_rev, max_rev))
+    plt.ylim((min_rat, max_rat))
+    plt.plot([mid_rev,mid_rev],[min_rat-1000,max_rat+1000], linewidth=1, color='gray' )
+    plt.plot([min_rev-1000,max_rev+1000],[mid_rat,mid_rat], linewidth=1, color='gray' )
 
-print("All cuisine types within the scope are defined in requirements document and readme.md file")
-user_input = input('Select a cuisine type - ')
-#nltk.download()
-#TODO: mention about nltk.download in requirements document
-cumm_bs_br = bs_data.merge(br_data, on = 'restaurant_id', how = 'inner')
-cumm_bs_br_ui = cumm_bs_br[cumm_bs_br['category'].str.lower() == user_input.lower()]
-#breakpoint()
-text_blocks = cumm_bs_br_ui['text']
-big_text = ' \n '
-for block in text_blocks:
-    big_text = big_text + block
+    plt.annotate('Leaders', xy=(mid_rev, mid_rat+0.02), fontsize = 6, c = 'red')
+    plt.annotate('Popular', xy=(mid_rev, min_rat+0.02), fontsize = 6, c = 'red')
+    plt.annotate('Highly Rated', xy=(min_rev, mid_rat+0.02), fontsize = 6, c = 'red')
+    plt.annotate('Promising', xy=(min_rev, min_rat+0.02), fontsize = 6, c = 'red')
+    plt.savefig(savefile('3 scatter.png'), bbox_inches='tight')
+    plt.clf()
+    #TODO: Clean up labels
+    ##############################################################################
+    ### Sentiment Anlayzer
+    # show people mood when they were giving feedback
+    sid = SentimentIntensityAnalyzer()
+    cat_txts = cumm_bs_br[['category', 'text']]
 
-#breakpoint()
+    sentiment_list = []
+    #sentiment_df = pd.DataFrame(columns=['cuisine_type', 'positive_score', 'negative_score'])
+    columns = ['cuisine_type', 'positive_score', 'negative_score']
+    i = 0
+    index_lst = cat_txts.index.values.tolist()
 
-tokens = nltk.word_tokenize(big_text)
-pos = nltk.pos_tag(tokens)
-adj_words = [p[0] for p in pos if p[1] in ['JJ', 'JJR', 'JJS']]
-noun_words = [p[0] for p in pos if p[1] in ['NN', 'NNP', 'NNS']]
-adj_str = ' '
-noun_str = ' '
-for s in adj_words:
-    adj_str = adj_str + ' ' + s
+    for i in index_lst:
+        review = cat_txts['text'][i]
+        sentiment = sid.polarity_scores(review)
+        #breakpoint()
+        row = [
+            cat_txts['category'][i],
+            sentiment['pos'],
+            sentiment['neg']
+        ]
+        sentiment_list.append(row)
 
-for n in noun_words:
-    noun_str = noun_str + ' ' + n
+    sentiment_df = pd.DataFrame.from_dict(sentiment_list)
+    sentiment_df.columns = columns
+    sentiment_df_agg = sentiment_df.groupby(['cuisine_type']).mean().reset_index()
 
-stopwords = set(STOPWORDS)
-stopwords.update(['\n', '/n', 'everything', 'review', 'indian', 'Indian', 'city', 'ok', 'city', 'went', 'couple', 'time', 'village', 'yelp' ])
-wc_adj = WordCloud(max_font_size=50, background_color = "white", max_words = 200, stopwords=stopwords).generate(adj_str)
-plt.figure()
-plt.imshow(wc_adj, interpolation = 'bilinear')
-plt.axis('off')
-plt.savefig(savefile('adjective_wc.png'), bbox_inches = 'tight')
+    y = sentiment_df_agg['cuisine_type']
+    x_pos = sentiment_df_agg['positive_score']
 
-wc_noun = WordCloud(max_font_size=50, background_color = "white", max_words = 200, stopwords=stopwords).generate(noun_str)
-plt.figure()
-plt.imshow(wc_noun, interpolation = 'bilinear')
-plt.axis('off')
-plt.savefig(savefile('noun_wc.png'), bbox_inches = 'tight')
-plt.clf()
+    # ajoined '-' because these are negative scores
+    x_neg = -sentiment_df_agg['negative_score']
 
-#breakpoint()
+    bars1 = plt.barh(y, x_pos, align='center', color = 'blue')
+    bars2 = plt.barh(y, x_neg, align='center', color = 'red')
+    plt.xlabel('Sentiment Score')
+    plt.ylabel('Cusine Type')
+    plt.title("Sentiment Analysis of user feedback")
+
+
+    for bar1, bar2 in zip(bars1, bars2):
+        xval1 = bar1.get_width()
+        plt.text(xval1,bar1.get_y()+0.01,float_format(xval1))
+        xval2 = bar2.get_width()
+        plt.text(xval2,bar2.get_y()+0.01,float_format(xval2))
+
+    plt.savefig(savefile('4 sentiment.png'), bbox_inches='tight')
+    plt.clf()
+
+    ################################################################################################
+    ### What do people like or dilike the mode, using word cloud
+    # Word Cloud for adjectives and Nouns
+    # User needs to run nltk.download()
+    # TODO: mention that user need to run import nltk; nltk.download() for wordcloud in requirement and help document
+
+    print("Please refer to README.MD file to identify the cusines within the scope of this project. To select all cusines, simply enter all")
+    user_input = input('Select a cuisine type - ')
+
+
+    if user_input.lower() == 'all':
+        text_blocks = cumm_bs_br['text']
+    elif user_input.lower() in cuisine_types:
+        cumm_bs_br_ui = cumm_bs_br[cumm_bs_br['category'].str.lower() == user_input.lower()]
+        text_blocks = cumm_bs_br_ui['text']
+    else:
+        print("Oops!! You entered an invalid value. Please refer to the README.MD file to identify cuisines within the scope.")
+        print("Run the program again")
+        sys.exit(5)
+
+    big_text = ' '
+    for block in text_blocks:
+        big_text = big_text + block
+
+    tokens = nltk.word_tokenize(big_text)
+    pos = nltk.pos_tag(tokens)
+    adj_words = [p[0] for p in pos if p[1] in ['JJ', 'JJR', 'JJS']]
+    noun_words = [p[0] for p in pos if p[1] in ['NN', 'NNP', 'NNS']]
+    adj_str = ' '
+    noun_str = ' '
+    for s in adj_words:
+        adj_str = adj_str + ' ' + s
+
+    for n in noun_words:
+        noun_str = noun_str + ' ' + n
+
+    stopwords = set(STOPWORDS)
+    stopwords.update(['\n', '/n', 'everything', 'single', 'overall', 'many', 'relative', 'review', 'indian', 'Indian', 'city', 'ok', 'city', 'went', 'couple', 'time', 'village', 'yelp' ])
+    wc_adj = WordCloud(max_font_size=50, background_color = "white", max_words = 200, stopwords=stopwords).generate(adj_str)
+    plt.figure()
+    plt.imshow(wc_adj, interpolation = 'bilinear')
+    plt.axis('off')
+    plt.savefig(savefile('5 adjective_wc.png'), bbox_inches = 'tight')
+
+    wc_noun = WordCloud(max_font_size=50, background_color = "white", max_words = 200, stopwords=stopwords).generate(noun_str)
+    plt.figure()
+    plt.imshow(wc_noun, interpolation = 'bilinear')
+    plt.axis('off')
+    plt.savefig(savefile('6 noun_wc.png'), bbox_inches = 'tight')
+    plt.clf()
+
+
 
 
 
